321cbfeb57ce844514922722adb8921b03ad5cef
laharipamidipati <65219495+laharipamidipati@users.noreply.github.com>
2020-05-17 17:22:29 +0530

    portfolio uploaded

---
+++ portfolio/script.js
@@ -0,0 +1,4 @@
+function openEmail() {
+    document.getElementById("mailId").classList.add("showEmail");
+    document.getElementById("mailIdText").innerHTML = "laharipamidipati2000@gmail.com";
+  }
